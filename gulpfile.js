2c4301b52c8fa5d3e980c2338b8c5068cc66594c
armando <armando@localhost.localdomain>
2017-02-11 11:53:16 -0600

    Se agregaron mas cosillas

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass'),
+	plumber = require('gulp-plumber');
 
 
 
-gulp.task('scripts', function(){
+gulp.task('js', function(){
 	gulp.src([
 		'./bower_components/jquery/dist/jquery.min.js',
-		'./src/script.js',
+		'./src/js/script.js',
 	])
+	.pipe(plumber())
 	.pipe(uglify())
 	.pipe(gulp.dest('./js'))
 
 
 });
 
+gulp.task('css', function() {
+	gulp.src('./src/scss/*.scss')
+	.pipe(plumber())
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}))
+	.pipe(gulp.dest('./css'));
+});
 
 
-gulp.task('default', ['scripts']);
+gulp.task('watch', function(){
+	gulp.watch('./src/scss/*.scss', ['css'])
+});
+
+gulp.task('default', ['js','css','watch']);
