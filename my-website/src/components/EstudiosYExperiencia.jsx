620c36f2818f0c2c0e67d0bee897385e3c9ddc14
Armando <armandojpr@yahoo.com>
2019-05-22 21:59:22 -0500

    Avance

--- my-website/src/components/EstudiosYExperiencia.jsx
+++ my-website/src/components/EstudiosYExperiencia.jsx
@@ -3,8 +3,38 @@ import React from 'react'
 
 const EstudiosYExperiencia = (props) => {
 
-  const dataFromJSON = props.jsonExperiencia.map((elem, index)=>{
-    return <p>{index} - {elem.position}</p>
+  const dataFromJSON = props.jsonExperiencia.map((item, index)=>{
+    return (
+      <div className="position">
+        <h3>{item.position} - {item.company}</h3>
+        <p className="fromTo">
+          From: {item.from} 
+          <br />
+          To: {item.to}
+        </p>
+        <>
+        {item.tasks.map((task) => {
+          return (
+            <>
+              <p>
+                {task.description}
+              </p>
+            
+              {task.details && (
+                <ul>
+                  {task.details.map(detail => {
+                    return <li>{detail}</li>
+                  })}
+                </ul>
+              )}
+            
+              
+            </>
+          )
+        })}
+        </>
+      </div>
+    )
   })
 
   return(
@@ -14,7 +44,7 @@ const EstudiosYExperiencia = (props) => {
           Estudios y experiencia
         </h2>
         <p>
-          Pues todo lo relacionado a mis estudios y experiencia esta en mi CV
+          A continuación se muestra mi experiencia todo lo relacionado a mis estudios y experiencia esta en mi CV
           <a href="../../public/docs/CVEsp.pdf" target="_blank">aqui</a>
           , y si lo que les interesa es en inglés pues aquí esta esta otra
           <a href="../../public/docs/CVEng.pdf" target="_blank">liga</a>
--- my-website/src/components/RedesSociales.jsx
+++ my-website/src/components/RedesSociales.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 
 const RedesSociales = () => (
     <div className="componentContainer componentContainer-redessociales">
-        redes sociales
+        <div class="twittwe">
+            <a href="https://twitter.com/bugdeveloper" target="_blank">Twitter</a>
+        </div>
+        <div>
+            <a href="https://twitter.com/bugdeveloper" target="_blank">Twitter</a>
+        </div>
     </div>
 )
 export default RedesSociales
