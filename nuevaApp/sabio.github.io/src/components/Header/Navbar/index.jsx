18a49924cc3abe3cbd92a895c3df1512fe3ca346
Armando Jesus Gomez Parra <armandojpr@yahoo.com>
2023-09-26 21:31:04 -0600

    Avance

--- nuevaApp/sabio.github.io/src/components/Header/Navbar/index.jsx
+++ nuevaApp/sabio.github.io/src/components/Header/Navbar/index.jsx
@@ -4,17 +4,20 @@ import { Link } from "react-router-dom";
 
 
 function Navbar() {
+    const setMenuActive = (checked) => {
+        document.getElementById('menuToggleCheckbox').checked = checked;
+    }
+
     return(
         <nav>
             <input id="menuToggleCheckbox" type="checkbox" value="checked" />
             <ul>
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/who-am-i">¿Quién soy?</Link></li>
-                <li><Link to="/experience">Educación y experiencia</Link></li>
+                <li><Link to="/" onClick={() => setMenuActive(false)}>Inicio</Link></li>
+                <li><Link to="/who-am-i" onClick={() => setMenuActive(false)}>¿Quién soy?</Link></li>
+                <li><Link to="/experience" onClick={() => setMenuActive(false)}>Educación y experiencia</Link></li>
             </ul>
         </nav>
     );
-
 }
 
 export default Navbar;
