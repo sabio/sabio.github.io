9108cbca1db696c244510b0df0c85aa179cb1c41
Armando Jesus Gomez Parra <armandojpr@yahoo.com>
2023-10-01 20:10:13 -0600

    Avance

--- nuevaApp/sabio.github.io/src/components/Header/Navbar/index.jsx
+++ nuevaApp/sabio.github.io/src/components/Header/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './index.scss';
 import { Link } from "react-router-dom";
+import { FormattedMessage } from 'react-intl'; 
 
 
 function Navbar() {
@@ -12,9 +13,9 @@ function Navbar() {
         <nav>
             <input id="menuToggleCheckbox" type="checkbox" value="checked" />
             <ul>
-                <li><Link to="/" onClick={() => setMenuActive(false)}>Inicio</Link></li>
-                <li><Link to="/who-am-i" onClick={() => setMenuActive(false)}>¿Quién soy?</Link></li>
-                <li><Link to="/experience" onClick={() => setMenuActive(false)}>Educación y experiencia</Link></li>
+                <li><Link to="/" onClick={() => setMenuActive(false)}><FormattedMessage id="app.home" /></Link></li>
+                <li><Link to="/who-am-i" onClick={() => setMenuActive(false)}><FormattedMessage id="app.whoami" /></Link></li>
+                <li><Link to="/experience" onClick={() => setMenuActive(false)}><FormattedMessage id="app.education.and.experience" /></Link></li>
             </ul>
         </nav>
     );
